5ead6c28c8e1c08df88fa84bcf257b30daddb574
Abhishek <abhishektripathi3u@gmail.com>
2019-06-16 11:42:22 +0530

    small bug

--- Algorithms/ViewController.swift
+++ Algorithms/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var result: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dcdcccc
         // Do any additional setup after loading the view, typically from a nib.
     }
 
